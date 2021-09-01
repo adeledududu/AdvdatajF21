674e6e388e2c1e470c7d0de82234e75b7b99e217
adeledududu <adeledu@mail.missouri.edu>
2021-09-01 00:28:53 -0500

    assignment1 revised

---
+++ data/assigment1/dispatchQs.R
@@ -0,0 +1,5 @@
+#1 What do geox and geoy stand for?
+#2 Why is there no information about AptLot?
+#3 What does PolArea stand for?
+#4 Why do some reports have numbers while others don't?
+#5 Is there a way to figure out which Extnaturedisplay has the highest frequency?
